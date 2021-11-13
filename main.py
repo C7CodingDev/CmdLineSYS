3cf5448739cd81572367f45da196ee0039288802
cool7coding <76494513+cooldudeseven7@users.noreply.github.com>
2021-11-13 11:04:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+for i in range(999999999999) :
+ CmdInpt =input('Command')
+ import os
+ from time import sleep as wait
+ os.system('clear')
+ if CmdInpt == 'Shutdown' :
+  print('Shutting Down...')
+  wait(0.79)
+  exit('Shutdown Complete')
+ else :
+	 print('Invalid!')
+	 wait(02.079)
+	 os.system('clear')
+    if CmdInpt == 'cmds' :
+    print('Shutdown = shutdown')
+    wait(0.79)
+   os.system ('clear')
+ else :
+	 print('Invalid!')
+	 wait(02.079)
+	 os.system('clear')
